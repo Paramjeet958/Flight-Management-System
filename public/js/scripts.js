be71e23b070acee45654a5142fb861377554a671
Paramjeet958 <82524633+Paramjeet958@users.noreply.github.com>
2024-06-04 19:08:56 -0400

    make page responsive

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,12 +9,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const headerHeight = mainNav.clientHeight;
     window.addEventListener('scroll', function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
+        if (currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
             } else {
-                console.log(123);
+               
                 mainNav.classList.remove('is-visible', 'is-fixed');
             }
         } else {
@@ -27,5 +27,50 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 
- 
-})
+    // Function to toggle return date field visibility
+    function toggleReturnDate() {
+        const returnDateField = document.getElementById('returnDateField');
+        const oneWayRadio = document.querySelector('input[name="tripType"][value="oneway"]');
+        returnDateField.style.display = oneWayRadio.checked ? 'none' : 'block';
+    }
+
+    // Call toggleReturnDate function when DOM content is loaded
+    toggleReturnDate();
+
+    // Event listener to toggle return date field visibility when trip type changes
+    document.querySelectorAll('input[name="tripType"]').forEach(radio => {
+        radio.addEventListener('change', toggleReturnDate);
+    });
+
+    // Counter update function
+    let experienceCounter = document.getElementById("experienceCounter");
+    let teamMembersCounter = document.getElementById("teamMembersCounter");
+    let clientsCounter = document.getElementById("clientsCounter");
+    let packagesCounter = document.getElementById("packagesCounter");
+
+    let experienceCount = 28;
+    let teamMembersCount = 600;
+    let clientsCount = 800;
+    let packagesCount = 999;
+
+    function updateCounters() {
+        experienceCounter.textContent = experienceCount++ + "+";
+        teamMembersCounter.textContent = teamMembersCount++ + "+";
+        clientsCounter.textContent = clientsCount++ + "+";
+        packagesCounter.textContent = packagesCount++ + "+";
+    }
+
+    setInterval(updateCounters, 10); // Update counters every second
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            } else {
+                entry.target.classList.remove('animate');
+            }
+        });
+    });
+
+    const centeredHeaders = document.querySelectorAll('.centered-header');
+    centeredHeaders.forEach(header => observer.observe(header));
+});
